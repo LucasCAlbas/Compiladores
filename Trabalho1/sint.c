4dc3fdc5e82bc8a02a5486ea675feb12295ed655
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-03-07 10:00:35 -0300

    Add files via upload

---
+++ Trabalho1/sint.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "analex.h"
+
+int token;
+
+void E();
+void T();
+void E_linha();
+void T_linha();
+void F();
+void erro();
+void reconhecer(int tok);
+
+void erro() {
+	printf("Erro sintatico\n");
+	exit(1);
+}
+
+void reconhecer(int tok) {
+	if (tok == token)
+	  token = analex();
+	else
+	  erro();
+}
+
+// implementar aqui
+void E(){
+    T();
+    E_linha();
+}
+
+main() {
+   token = analex();
+   E();
+   if (token != ';')
+     erro();
+   else
+     printf("Sucesso!!\n");
+}
+
